a28fceebea336d552e2f3fe5d8447e470829a239
tinywell <zfh-zyw@163.com>
2023-11-11 18:34:35 +0800

    支持 openai 代理

--- src/hooks/useChatGPT.tsx
+++ src/hooks/useChatGPT.tsx
@@ -1,4 +1,5 @@
 import { getPreferenceValues } from "@raycast/api";
+import { get } from "http";
 import { Configuration, OpenAIApi } from "openai";
 import { useState } from "react";
 
@@ -9,6 +10,20 @@ export function useChatGPT(): OpenAIApi {
     }>().api;
 
     const config = new Configuration({ apiKey });
+
+    const baseAPI = getPreferenceValues<{
+      baseAPI: string;
+    }>().baseAPI;
+
+    const useBaseAPI = getPreferenceValues<{
+      useBaseAPI: boolean;
+    }>().useBaseAPI;
+
+    if (useBaseAPI) {
+      config.basePath = baseAPI;
+    }  
+
+    // console.log(config);
 
     return new OpenAIApi(config);
   });
